0a780becf5355b141d99d5e6a26a8ab55d4d6cb6
karleighponnock <karleighponnock@gmail.com>
2020-07-14 20:19:39 -0400

    readme updated with tech frameworks

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,18 +1,20 @@
 var path = require('path');
 var express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
+const { v1: uuidv1 } = require('uuid');
 
 //exports files to server file reqirements
 module.exports = function() {
 
     app.get("/api/notes", function (req, res) {
-        console.log("hello world")
         res.json(req.body)
         return res.json(notes);
     });
 
     app.post("/api/notes", function (req, res) {
         var newNote = req.body
+        console.log(uuidv1())
         notes.push(newNote)
         res.json(newNote)
         res.json(req.body)
@@ -20,8 +22,7 @@ module.exports = function() {
     });
 
     app.delete("/api/notes", function (req, res) {
-
-
+        handleNoteDelete()
         res.json(req.body)
         return res.json(notes);
     });
